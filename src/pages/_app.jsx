56dbf76a10026baeec2676ff47e2c5e426617712
심재성 <shimjs8@naver.com>
2020-03-24 07:37:21 +0900

    refactor(style): no document title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,11 @@
+import { Head } from 'react';
 import '../styles/globalStyle.scss';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+export default ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Coin - 같이의 가치를 믿다</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
